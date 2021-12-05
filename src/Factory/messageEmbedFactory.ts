c709ec26ca661fef36e3af32edcf2331b56958c2
its-treason <39559178+Its-treason@users.noreply.github.com>
2021-12-05 21:42:31 +0100

    fix: Fix TS build errors

--- src/Factory/messageEmbedFactory.ts
+++ src/Factory/messageEmbedFactory.ts
@@ -4,7 +4,7 @@ export default (client: Client, title: string): MessageEmbed => {
   const answer = new discord.MessageEmbed();
   answer.setColor('#b4dbe0');
   answer.setTimestamp(new Date());
-  answer.setFooter(`${client.user?.username}`, client.user?.avatarURL());
+  answer.setFooter(`${client.user?.username}`, client.user?.avatarURL() || undefined);
   answer.setTitle(title);
 
   return answer;
